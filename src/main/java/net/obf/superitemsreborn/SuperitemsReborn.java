6ea83aaafce5e966583b10b3b5685596eb4819c4
ninjaguardian <cjfairchild51@gmail.com>
2024-05-27 11:34:54 -0500

    Initial commit

---
+++ src/main/java/net/obf/superitemsreborn/SuperitemsReborn.java
@@ -0,0 +1,17 @@
+package net.obf.superitemsreborn;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SuperitemsReborn extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
